21651551be5068f04b7276927535a2ec217b18ad
Стас Затушевский <STAs20160@yandex.ru>
2025-06-16 20:48:10 +0300

    bug fix

--- cmd/server/internal/database/storage_test.go
+++ cmd/server/internal/database/storage_test.go
@@ -1,9 +1,7 @@
 package database
 
 import (
-	"errors"
 	"github.com/stretchr/testify/assert"
-	"net/url"
 	"testing"
 )
 
@@ -29,64 +27,4 @@ func TestMemStorage_SetGetCounter(t *testing.T) {
 	ms.SetCounter("counter", int64(3))
 	resSet, _ := ms.GetCounter("counter")
 	assert.Equal(t, int64(4), resSet)
-}
-
-func TestParseData(t *testing.T) {
-	type want struct {
-		nameMetric string
-		dataMetric string
-		typeMetric string
-		err        error
-	}
-	tests := []struct {
-		name string
-		url  string
-		want want
-	}{
-		{
-			name: "success",
-			url:  "http://localhost/update/counter/a/1",
-			want: want{
-				nameMetric: "a",
-				dataMetric: "1",
-				typeMetric: "counter",
-				err:        nil,
-			},
-		},
-		{
-			name: "wrong url",
-			url:  "http://localhost/1",
-			want: want{
-				nameMetric: "",
-				dataMetric: "",
-				typeMetric: "",
-				err:        errors.New("invalid URL path: /1"),
-			},
-		},
-		{
-			name: "missing nameMetric",
-			url:  "http://localhost/update/counter//1",
-			want: want{
-				nameMetric: "",
-				dataMetric: "",
-				typeMetric: "",
-				err:        errors.New("missing metric name in URL"),
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			u, _ := url.Parse(tt.url)
-			gotName, gotData, gotType, gotErr := ParseData(u.Path)
-
-			assert.Equal(t, tt.want.nameMetric, gotName)
-			assert.Equal(t, tt.want.dataMetric, gotData)
-			assert.Equal(t, tt.want.typeMetric, gotType)
-			if tt.want.err != nil {
-				assert.EqualError(t, gotErr, tt.want.err.Error())
-			} else {
-				assert.NoError(t, gotErr)
-			}
-		})
-	}
 }
